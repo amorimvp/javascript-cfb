6788536d2cb35ef34f0341ecfad0162bb5d90a69
amorim <>
2023-08-15 15:48:29 -0300

    up

---
+++ aula01/aula01.js
@@ -0,0 +1,3 @@
+console.log("Esta linha está sendo comentada")
+console.log("Alô, Mundo")
+console.log("Fim")
---
+++ aula03/aula03.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let nome = "Bruno" //let so funciona dentro do mesmo escopo
+nome = "CFB Cursos"
+nome = 10
+
+const curso = "JavaScript" //Const é impossivel mudar o valor
+
+console.log(curso)
